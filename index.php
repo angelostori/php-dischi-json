9f5e7d54c588797705b780974ca554e525e79935
angelostori <angelostori13@gmail.com>
2026-02-12 11:38:47 +0100

    improve data apparence using cards responsive

--- index.php
+++ index.php
@@ -9,16 +9,32 @@ require_once "./functions.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Raccolta Musicale</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
     <h1>Raccolta Musicale</h1>
 
-    <ul>
-        <?php
-        foreach($records as $record) {
-            echo "<li>" . $record['title'] . " - " . $record['artist'] . "</li>";
-        }
-        ?>
-    </ul>
+
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3 g-3">
+            <?php foreach($records as $record) { ?>    
+            <div class="col">
+                <div class="card">
+                    <img 
+                        class="card-img-top p-1" 
+                        src=<?php echo $record['cover_url'] ?> 
+                        alt=<?php echo $record['title'] . ' - ' . $record['artist'] ?> />
+                    <div class="card-body">
+                        <h4 class="card-title"><?php echo $record['title'] ?></h4>
+                        <p class="card-text"><?php echo $record['artist'] ?></p>
+                        <p class="card-text text-muted"><?php echo $record['release_date'] . ' - ' . $record['genre']?></p>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+        
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
